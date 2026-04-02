7df9825064bff2b2f077241b87f14bff2216cd1a
bhujelrojan <bhujelrojan34@gmail.com>
2026-04-02 15:48:56 +0300

    Add files via upload

--- 2504651.js
+++ 2504651.js
@@ -1,8 +1,13 @@
 const reader = new Html5Qrcode("camera");
 let scannerOn = false;
 
-function toggleScanner() {
+const mapContainer = document.getElementById("mapContainer");
+const btn = document.getElementById("btn");
+const marker = document.getElementById("marker");
+
+function toggleScanner(){
     scannerOn = !scannerOn;
+
     if (scannerOn) {
         startScanner();
         mapContainer.style.display = "none";
@@ -19,29 +24,27 @@ function startScanner() {
         { facingMode: "environment" },
         {},
         function (text) {
-            const item = JSON.parse(text);
+            const place = JSON.parse(text);
+            const top = place.latitude * 5 + "px";
+            const left = place.longitude * 5 + "px";
 
-            // Show marker (keep existing functionality)
-            showMarkerAt(item.top, item.left,place.name,place.latitude,place.longitude);
-
-            // Show inventory info
-            displayItemInfo(item);
-
+            showMarkerAt(top, left, place.name, place.latitude, place.longitude);
             toggleScanner();
         }
-    ).catch(function (err) {
+    ).catch(function(err){
         console.error(err);
     });
 }
-
-
-
 
 function stopScanner() {
     reader.stop();
 }
 
-function showMarkerAt(top, left) {
+function showMarkerAt(top, left, name, latitude, longitude) {
     marker.style.top = top;
     marker.style.left = left;
+
+    document.getElementById("itemName").innerText = "Name: " + name;
+    document.getElementById("itemLatitude").innerText = "Latitude: " + latitude;
+    document.getElementById("itemLongitude").innerText = "Longitude: " + longitude;
 }
